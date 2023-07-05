33ae24b2c6e52421cbc456387c0907f83b9d353c
nailadira <nailanadira97@gmail.com>
2023-07-05 16:26:14 +0700

    mengubah isi file makam.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,27 +1,20 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
+class Dashboard extends CI_Controller
+{
 
 	public function index()
 	{
 		$data['makam'] = $this->db->query("SELECT * FROM makam r ORDER BY id_makam DESC")->result();
 		$data['paket'] = $this->db->query("SELECT * FROM paket f, makam r WHERE f.id_makam=r.id_makam ORDER BY id_paket DESC")->result();
 		$data['title'] = 'Siforma';
-		$this->load->view('templates_home/header',$data);
+		$this->load->view('templates_home/header', $data);
 		$this->load->view('home/index');
 		$this->load->view('templates_home/footer');
 	}
 
-	public function paket_detail($id)
-	{
-		$data['detail'] = $this->db->query("SELECT * FROM paket f, makam r WHERE f.id_makam=r.id_makam AND f.id_paket='$id' ORDER BY id_paket DESC")->result();
-		$data['title'] = 'Detail Paket | Siforma';
-		$this->load->view('templates_home/header',$data);
-		$this->load->view('home/paket_detail');
-		$this->load->view('templates_home/footer');
-	}
-
+	
 }
 
 /* End of file Dashboard.php */
